ba308159691e747dcc46db88112249a7ba086688
NguyenChauTinh <chautinh05122@gmail.com>
2024-08-19 16:06:04 +0700

    Chanllenge 3: Find team win Dolphins and Koalas

--- Lab1/Challenge3/app.js
+++ Lab1/Challenge3/app.js
@@ -1,17 +1,42 @@
-markHigherBMI = false;
-massMark = 78;
-heightMark = 1.69;
+dophinsScore = [96, 108, 89];
+koalasScore = [88, 91, 110];
 
-massJohn = 92;
-heightJohn = 1.95;
-
-BMIMark = massMark / heightMark ** 2;
-BMIJohn = massJohn / heightJohn ** 2;
-
-if (BMIMark > BMIJohn) {
-    markHigherBMI = true;
-    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
-    
-} else {
-    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+const calcAverage = (score) => {
+    return score.reduce((a, b) => a + b) / score.length;
 }
+
+var avgDolphins = calcAverage(dophinsScore);
+var avgKoalas = calcAverage(koalasScore);
+
+const checkWinner = (avgDolphins, avgKoalas) => {
+    if (avgDolphins > avgKoalas && avgDolphins >= 100) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas > avgDolphins && avgKoalas >= 100) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log(`No team wins`);
+    }
+}
+
+console.log('Times 1:');
+
+checkWinner(avgDolphins, avgKoalas);
+
+dophinsScore = [97, 112, 101];
+koalasScore = [109, 95, 123];
+
+avgDolphins = calcAverage(dophinsScore);
+avgKoalas = calcAverage(koalasScore);
+
+console.log('Times 2:');
+
+checkWinner(avgDolphins, avgKoalas);
+
+dophinsScore = [97, 112, 101];
+koalasScore = [109, 95, 106];
+
+avgDolphins = calcAverage(dophinsScore);
+avgKoalas = calcAverage(koalasScore);
+
+console.log('Times 3:');
+checkWinner(avgDolphins, avgKoalas);
