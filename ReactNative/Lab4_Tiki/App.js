a9e7ff193d255ce31edaacef347cbfeb97271f31
NguyenChauTinh <chautinh05122@gmail.com>
2024-09-16 21:05:53 +0700

    add Lab4 Tiki

---
+++ ReactNative/Lab4_Tiki/App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <Header />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ ReactNative/Lab4_Tiki/components/Header.js
@@ -0,0 +1,12 @@
+import{ View, Text, StyleSheet } from 'react-native';
+
+function Header() {
+    return ( 
+        <View >
+            <Text >Tiki</Text>
+        </View>
+
+     );
+}
+
+export default Header;
